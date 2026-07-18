479754a9df0a89d7b626fd5d62f0aaf6a335cd9b
SGolden58 <super.golden.golden@gmail.com>
2026-07-18 16:37:33 +0800

    Update viutv.py

--- viutv.py
+++ viutv.py
@@ -12,8 +12,8 @@ class ViuTVPlatform:
     async def fetch_all_programs(self, days=2):
         all_programs = []
         target_map = {
-            "ViuTV.hk": "ViuTV",
-            "ViuTVsix.hk": "ViuTVsix"
+            "ViuTV.hk": "099",
+            "ViuTVsix.hk": "096"
         }
         
         try:
