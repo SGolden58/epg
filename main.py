6936af57b710fe2b3df279bb3a957c66554abc9c
SGolden58 <super.golden.golden@gmail.com>
2026-07-19 04:52:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def run_all():
         add_custom_section(root, cid, hoy_progs)
 
     # 2. Add ViuTV Channels (99, 96)
-    for cid in ["99", "96"]:
+    for cid in ["099", "096"]:
         add_custom_section(root, cid, viu_progs)
 
     # 3. Add epg.pw Channels
